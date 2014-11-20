ff1ef42a79a785ff354cfcd24d3e4cf4e55f1f5e
Gerald A. Wakefield <gwake1@gmail.com>
2014-11-20 16:03:07 -0600

    WIP basic setup for Test Development

---
+++ spec/test.js
@@ -0,0 +1,10 @@
+/* global describe, it */
+
+function(){
+  'use strict';
+  describe('Prepare for Battle', function(){
+    it("should return player 1", function (){
+      assert.equal("player1")
+    })
+  });
+}
