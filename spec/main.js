5eae6bb16ae80742f2ad15e485ab1a5a5742ad04
Gerald A. Wakefield <gwake1@gmail.com>
2014-11-21 09:21:59 -0600

    Fixed ReadME.md and added basic css and assets

--- spec/main.js
+++ spec/main.js
@@ -1,5 +1,33 @@
-var playerTurn = false;
+var player = {
+  player1: {
+    name: "David",
+    turn: true,
+  },
+  player2: {
+    name: "Jackie",
+    turn: false,
+  }
+};
+var game = {
+  turn: true,
+}
 
 function playerName(){
-  return "David"
+  if(game.turn === true){
+    return player.player1.name;
+  } else {
+    return player.player2.name;
+  }
+}
+
+function playerTurn(){
+  if(game.turn === true){
+    return player.player1.name;
+    game.turn = false;
+    player.player1.turn = false;
+    player.player2.turn = true;
+  } else {
+    return player2;
+    game.turn = true;
+  }
 }
--- spec/test.js
+++ spec/test.js
@@ -1,9 +1,22 @@
 (function (){
   "use strict";
-})
+});
 
 describe("Player Name Log", function(){
   it("should be David", function(){
     assert.equal(playerName(), "David")
-  })
-})
+  });
+});
+
+describe("set Player turn", function (){
+  describe("at beginning player 1 should move first", function(){
+    it("player1 turn should be set to true", function(){
+      assert.equal(player.player1.turn, true)
+    });
+  });
+  describe("at beginning player 2 should not be able to move", function(){
+    it("player2 turn should be set to false", function(){
+      assert.equal(player.player2.turn, false)
+    })
+  });
+});
