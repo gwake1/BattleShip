e76c64d00ce0485e8ddc97e6b8d469bda39615c4
jessicafraines <jessicafraines@gmail.com>
2014-11-24 13:13:17 -0600

    Added revert & rotate functionality and cleaned up test

--- spec/main.js
+++ spec/main.js
@@ -1,7 +1,15 @@
 /* jshint jquery: true, browser: true */
-
+/*var myDataRef = new Firebase('https://battleshipnss.firebaseio.com/');
+      $('#messageInput').keypress(function (e) {
+        if (e.keyCode == 13) {
+          var name = $('#nameInput').val();
+          var text = $('#messageInput').val();
+          myDataRef.set('User ' + name + ' says ' + text);
+          $('#messageInput').val('');
+        }
+      });*/
 var player = {
-  player1: {
+player1: {
     name: "Jessica",
     turn: true,
     show: true,
@@ -78,4 +86,11 @@ function hidePlayer2 (){
   console.log("player2 shown")
 }
 
-  $("#draggable").draggable({grid:[50,50]});
+$("#draggable").draggable({grid:[50,50], revert: "invalid"});
+$(".piecesGrid").droppable();
+
+var angle = 0;
+$('#button').on('click', function() {
+  angle += 90;
+  $(".battleship").rotate(angle);
+});
